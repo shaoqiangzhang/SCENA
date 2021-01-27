73d409ea506ed8caa5f43ae96b53cf0b543c5010
Shaoqiang ZHANG <35386642+shaoqiangzhang@users.noreply.github.com>
2021-01-28 00:01:59 +0800

    Add files via upload

--- R/ApSpe_GPU.R
+++ R/ApSpe_GPU.R
@@ -1,6 +1,10 @@
+
+
+
 select_features_gpu=function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250){
   library(SNFtool)
   library(apcluster)
+  alpha=0.5
   .discretisation <- function(eigenVectors) {
 
     normalize <- function(x) x / sqrt(sum(x^2))
@@ -77,12 +81,132 @@ select_features_gpu=function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250){
 
     return(normalize(A))
   }
+  
+  
+  
+  KNN_SMI_GPU <- function(W,K=20,t=20) {
+
+    library(gpuR)
+
+    .discretisation <- function(eigenVectors) {
+
+      normalize <- function(x) x / sqrt(sum(x^2))
+      eigenVectors = t(apply(eigenVectors,1,normalize))
+
+      n = nrow(eigenVectors)
+      k = ncol(eigenVectors)
+
+      R = matrix(0,k,k)
+      R[,1] = t(eigenVectors[round(n/2),])
+
+      mini <- function(x) {
+        i = which(x == min(x))
+        return(i[1])
+      }
+
+      c = matrix(0,n,1)
+      for (j in 2:k) {
+        c = c + abs(eigenVectors %*% matrix(R[,j-1],k,1))
+        i = mini(c)
+        R[,j] = t(eigenVectors[i,])
+      }
+
+      lastObjectiveValue = 0
+      for (i in 1:20) {
+        eigenDiscrete = .discretisationEigenVectorData(eigenVectors %*% R)
+
+        svde = svd(t(eigenDiscrete) %*% eigenVectors)
+        U = svde[['u']]
+        V = svde[['v']]
+        S = svde[['d']]
+
+        NcutValue = 2 * (n-sum(S))
+        if(abs(NcutValue - lastObjectiveValue) < .Machine$double.eps)
+          break
+
+        lastObjectiveValue = NcutValue
+        R = V %*% t(U)
+
+      }
+
+      return(list(discrete=eigenDiscrete,continuous =eigenVectors))
+    }
+
+    .discretisationEigenVectorData <- function(eigenVector) {
+
+      Y = matrix(0,nrow(eigenVector),ncol(eigenVector))
+      maxi <- function(x) {
+        i = which(x == max(x))
+        return(i[1])
+      }
+      j = apply(eigenVector,1,maxi)
+      Y[cbind(1:nrow(eigenVector),j)] = 1
+
+      return(Y)
+
+    }
+
+    .dominateset <- function(xx,KK=20) {
+      ###This function outputs the top KK neighbors.
+
+      zero <- function(x) {
+        s = sort(x, index.return=TRUE)
+        x[s$ix[1:(length(x)-KK)]] = 0
+        return(x)
+      }
+      normalize <- function(X) X / rowSums(X)
+      A = matrix(0,nrow(xx),ncol(xx));
+      for(i in 1:nrow(xx)){
+        A[i,] = zero(xx[i,]);
+
+      }
+
+
+      return(normalize(A))
+    }
+
+
+
+    normalize <- function(X) X / rowSums(X)
+
+    newW =matrix(0,dim(W)[1], dim(W)[2])
+    nextW =matrix(0,dim(W)[1], dim(W)[2])
+    W = normalize(W);
+    W = (W+t(W))/2;
+    newW = (.dominateset(W,K))
+    nextW=W;
+    gpuA<-gpuMatrix(nextW,type = "double")
+    gpuB<-gpuMatrix(newW ,type = "double")
+    for (i in 1:t) {
+      gpuA<-gpuB %*% (gpuA) %*% t(gpuB)
+    }
+
+    for (i in 1:nrow(gpuA)) {
+      for (j in 1:ncol(gpuA)) {
+        nextW[i,j]=gpuA[i,j]
+      }
+    }
+    for (i in 1:nrow(gpuB)) {
+      for (j in 1:ncol(gpuB)) {
+        newW[i,j]=gpuB[i,j]
+      }
+    }
+    W = nextW + diag(nrow(W));
+    W = (W + t(W))/2;
+
+
+    return(W)
+  }
+  
+  
+  
   num=apply(Express, 1, sd)
   Express=as.array(Express)
   num=as.array(num)
   Total<-cbind(num,Express)
   Total1<-Total[order(Total[,1],decreasing = TRUE),]
   if(X==1){
+    alpha=0.5
     Total1=Total1[1:X1,]
     Total12=Total1[,-1]
     Total12=apply(Total12,1,as.numeric)
@@ -98,6 +222,7 @@ select_features_gpu=function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250){
     write.table(group1,file="data1.txt",sep=" ",quote=TRUE)
   }
   if(X==2){
+    alpha=0.5
     Total1=Total1[1:X2,]
     Total22=Total1[,-1]
     Total22=apply(Total22,1,as.numeric)
@@ -113,6 +238,7 @@ select_features_gpu=function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250){
     write.table(group2,file="data2.txt",sep=" ",quote=TRUE)
   }
   if(X==3){
+    alpha=0.5
     Total1=Total1[1:X3,]
     Total32=Total1[,-1]
     Total32=apply(Total32,1,as.numeric)
@@ -128,6 +254,7 @@ select_features_gpu=function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250){
     write.table(group3,file="data3.txt",sep=" ",quote=TRUE)
   }
   if(X==4){
+    alpha=0.5
     Total1=Total1[1:X4,]
     Total42=Total1[,-1]
     Total42=apply(Total42,1,as.numeric)
@@ -143,6 +270,7 @@ select_features_gpu=function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250){
     write.table(group4,file="data4.txt",sep=" ",quote=TRUE)
   }
   if(X==5){
+    alpha=0.5
     Total1=Total1[1:X5,]
     Total52=Total1[,-1]
     Total52=apply(Total52,1,as.numeric)
