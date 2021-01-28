61c17cce6d5619989961477277c07c1618aea79f
Shaoqiang ZHANG <35386642+shaoqiangzhang@users.noreply.github.com>
2021-01-28 11:24:46 +0800

    GPU version

--- R/ApSpe_GPU.R
+++ R/ApSpe_GPU.R
@@ -1,86 +1,7 @@
-select_features_gpu=function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250, Express=Express){
+select_features_GPU = function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250,Express=Express){
   library(SNFtool)
   library(apcluster)
   alpha=0.5
-  .discretisation <- function(eigenVectors) {
-
-    normalize <- function(x) x / sqrt(sum(x^2))
-    eigenVectors = t(apply(eigenVectors,1,normalize))
-
-    n = nrow(eigenVectors)
-    k = ncol(eigenVectors)
-
-    R = matrix(0,k,k)
-    R[,1] = t(eigenVectors[round(n/2),])
-
-    mini <- function(x) {
-      i = which(x == min(x))
-      return(i[1])
-    }
-
-    c = matrix(0,n,1)
-    for (j in 2:k) {
-      c = c + abs(eigenVectors %*% matrix(R[,j-1],k,1))
-      i = mini(c)
-      R[,j] = t(eigenVectors[i,])
-    }
-
-    lastObjectiveValue = 0
-    for (i in 1:20) {
-      eigenDiscrete = .discretisationEigenVectorData(eigenVectors %*% R)
-
-      svde = svd(t(eigenDiscrete) %*% eigenVectors)
-      U = svde[['u']]
-      V = svde[['v']]
-      S = svde[['d']]
-
-      NcutValue = 2 * (n-sum(S))
-      if(abs(NcutValue - lastObjectiveValue) < .Machine$double.eps)
-        break
-
-      lastObjectiveValue = NcutValue
-      R = V %*% t(U)
-
-    }
-
-    return(list(discrete=eigenDiscrete,continuous =eigenVectors))
-  }
-
-  .discretisationEigenVectorData <- function(eigenVector) {
-
-    Y = matrix(0,nrow(eigenVector),ncol(eigenVector))
-    maxi <- function(x) {
-      i = which(x == max(x))
-      return(i[1])
-    }
-    j = apply(eigenVector,1,maxi)
-    Y[cbind(1:nrow(eigenVector),j)] = 1
-
-    return(Y)
-
-  }
-
-  .dominateset <- function(xx,KK=20) {
-    ###This function outputs the top KK neighbors.
-
-    zero <- function(x) {
-      s = sort(x, index.return=TRUE)
-      x[s$ix[1:(length(x)-KK)]] = 0
-      return(x)
-    }
-    normalize <- function(X) X / rowSums(X)
-    A = matrix(0,nrow(xx),ncol(xx));
-    for(i in 1:nrow(xx)){
-      A[i,] = zero(xx[i,]);
-
-    }
-
-
-    return(normalize(A))
-  }
-  
-  
-  
   KNN_SMI_GPU <- function(W,K=20,t=20) {
 
     library(gpuR)
@@ -194,9 +115,82 @@ select_features_gpu=function(X,K=10,T=100,X1=50,X2=100,X3=150,X4=200,X5=250, Exp
 
     return(W)
   }
-  
-  
-  
+  .discretisation <- function(eigenVectors) {
+
+    normalize <- function(x) x / sqrt(sum(x^2))
+    eigenVectors = t(apply(eigenVectors,1,normalize))
+
+    n = nrow(eigenVectors)
+    k = ncol(eigenVectors)
+
+    R = matrix(0,k,k)
+    R[,1] = t(eigenVectors[round(n/2),])
+
+    mini <- function(x) {
+      i = which(x == min(x))
+      return(i[1])
+    }
+
+    c = matrix(0,n,1)
+    for (j in 2:k) {
+      c = c + abs(eigenVectors %*% matrix(R[,j-1],k,1))
+      i = mini(c)
+      R[,j] = t(eigenVectors[i,])
+    }
+
+    lastObjectiveValue = 0
+    for (i in 1:20) {
+      eigenDiscrete = .discretisationEigenVectorData(eigenVectors %*% R)
+
+      svde = svd(t(eigenDiscrete) %*% eigenVectors)
+      U = svde[['u']]
+      V = svde[['v']]
+      S = svde[['d']]
+
+      NcutValue = 2 * (n-sum(S))
+      if(abs(NcutValue - lastObjectiveValue) < .Machine$double.eps)
+        break
+
+      lastObjectiveValue = NcutValue
+      R = V %*% t(U)
+
+    }
+
+    return(list(discrete=eigenDiscrete,continuous =eigenVectors))
+  }
+
+  .discretisationEigenVectorData <- function(eigenVector) {
+
+    Y = matrix(0,nrow(eigenVector),ncol(eigenVector))
+    maxi <- function(x) {
+      i = which(x == max(x))
+      return(i[1])
+    }
+    j = apply(eigenVector,1,maxi)
+    Y[cbind(1:nrow(eigenVector),j)] = 1
+
+    return(Y)
+
+  }
+
+  .dominateset <- function(xx,KK=20) {
+    ###This function outputs the top KK neighbors.
+
+    zero <- function(x) {
+      s = sort(x, index.return=TRUE)
+      x[s$ix[1:(length(x)-KK)]] = 0
+      return(x)
+    }
+    normalize <- function(X) X / rowSums(X)
+    A = matrix(0,nrow(xx),ncol(xx));
+    for(i in 1:nrow(xx)){
+      A[i,] = zero(xx[i,]);
+
+    }
+
+
+    return(normalize(A))
+  }
   num=apply(Express, 1, sd)
   Express=as.array(Express)
   num=as.array(num)
