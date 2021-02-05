ee2760e26f4e360ec49a2fec44b385599a7e37b9
Shaoqiang ZHANG <35386642+shaoqiangzhang@users.noreply.github.com>
2021-02-05 14:44:55 +0800

    Add files via upload

---
+++ R/plotpca.R
@@ -0,0 +1,10 @@
+plotPCA<-function(Express,cc){
+  library(ggplot2)
+  #library(RColorBrewer)
+  Exp.pca <- princomp(Express, cor=TRUE, scores=TRUE)
+  Exp.pca=prcomp(t(Express),center = TRUE,scale. = TRUE)
+   
+  Clusters=as.factor(cc)
+  ggplot(as.data.frame(Exp.pca$x),aes(x=PC1,y=PC2,colour=Clusters)) + 
+  geom_point()+theme_bw()
+}
